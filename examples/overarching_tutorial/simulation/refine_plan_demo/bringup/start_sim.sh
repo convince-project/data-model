27c8ae2aab5b8e867f25735cb74bdbab2b5e086b
Charlie Street <me@charliestreet.net>
2026-02-19 14:17:16 +0000

    Add parameter to change detect prob in overarching demo
    Signed-off-by: Charlie Street <me@charliestreet.net>

--- examples/overarching_tutorial/simulation/refine_plan_demo/bringup/start_sim.sh
+++ examples/overarching_tutorial/simulation/refine_plan_demo/bringup/start_sim.sh
@@ -11,7 +11,7 @@ tmux rename-window -t $SESSION 'pyrobosim'
 tmux new-window -t $SESSION -n 'policy-executor'
 
 tmux select-window -t $SESSION:pyrobosim
-tmux send-keys "ros2 run tutorial_sim run" C-m
+tmux send-keys "ros2 run tutorial_sim run --ros-args -p detect_succ_prob:=1.0" C-m
 
 tmux select-window -t $SESSION:policy-executor
 tmux send-keys "ros2 launch refine_plan_demo policy_executor.launch.py db_collection:=demo-$POLICY_MODE mode:=$POLICY_MODE db_connection_string:=$MONGO_CONNECTION_STRING" C-m
--- examples/overarching_tutorial/simulation/tutorial_sim/tutorial_sim/run.py
+++ examples/overarching_tutorial/simulation/tutorial_sim/tutorial_sim/run.py
@@ -18,13 +18,28 @@ def create_ros_node():
     """Initializes ROS node"""
     rclpy.init()
     node = WorldROSWrapper(state_pub_rate=0.1, dynamics_rate=0.01)
-    
+
+    # Allow for detect success probability to be overwritten
+    # This is for the REFINE-PLAN demo only
+    # I'd rather do this than change the world file
+    node.declare_parameter("detect_succ_prob", rclpy.Parameter.Type.DOUBLE)
+    detect_prob = node.get_parameter_or("detect_succ_prob", alternative_value=None)
+
     world_file = os.path.join(
         get_package_share_directory("tutorial_sim"),
         "worlds",
         "world.yaml",
     )
     world = WorldYamlLoader().from_file(world_file)
+
+    if detect_prob is not None:
+        node.get_logger().info(
+            f"Overriding detect success probability to: {detect_prob.value}"
+        )
+        robot_name = world.get_robot_names()[0]
+        robot = world.get_robot_by_name(robot_name)
+        robot.action_execution_options["detect"].success_probability = detect_prob.value
+
     # world.reset(seed=0)
     world.reset()  # This randomizes the world.
     node.set_world(world)
